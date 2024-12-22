7001b472d5ce62150fdef5024a35633984320b0f
Theresa <theresari.100@gmail.com>
2024-12-22 23:03:18 +0100

    new cat-card added and colors adjusted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
+import { CatCardComponent } from './cat-card/cat-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, CatCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 
---
+++ src/app/cat-card/cat-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cat-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './cat-card.component.html',
+  styleUrl: './cat-card.component.scss'
+})
+export class CatCardComponent {
+
+}
