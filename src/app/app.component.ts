d47f1adfd24d8c5082601b865b5e332100e01fec
Theresa <theresari.100@gmail.com>
2024-12-23 19:20:28 +0100

    footer & more cats added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,45 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
-import { CatCardComponent } from './cat-card/cat-card.component';
 import { CommonModule } from '@angular/common';
+
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CatCardComponent } from './cat-card/cat-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, CatCardComponent, CommonModule],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    HeaderComponent,
+    CatCardComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  postName = ['Luna', 'Milo', 'Kitty', 'Hugo', 'Oreo'];
+  postName = [
+    'Luna',
+    'Milo',
+    'Kitty',
+    'Hugo',
+    'Oreo',
+    'Bella',
+    'Billy',
+    'Nala',
+  ];
+
   postImages = [
     'assets/img/cats/1.jpg',
     'assets/img/cats/2.jpg',
     'assets/img/cats/3.jpg',
     'assets/img/cats/4.jpg',
     'assets/img/cats/5.jpg',
+    'assets/img/cats/6.jpg',
+    'assets/img/cats/7.jpg',
+    'assets/img/cats/8.jpg',
   ];
 
   buttonClice() {
